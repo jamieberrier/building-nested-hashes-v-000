cf0be403bed87dc69f610d8df3928f654ef826da
jamieberrier <berrier.jamie@gmail.com>
2018-08-05 04:33:36 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,7 +3,8 @@ def third_challenge
    :montague => {
       :patriarch => {
         #your key/value pairs here
-        :name => "Lord Montague"
+        :name => "Lord Montague",
+        :age => "53"
         },
       :matriarch => {
         #your key/value pairs here
@@ -12,7 +13,7 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -27,5 +28,5 @@ def third_challenge
    }
   }
 
-  
+
 end
